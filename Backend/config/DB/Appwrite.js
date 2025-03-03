6ac0cba6230882a0d0450a17473f57f222620a7e
Jay Fanse <99622128+JayFanse29@users.noreply.github.com>
2025-03-03 14:22:38 +0530

    Delete Backend directory

--- Backend/config/DB/Appwrite.js
+++
@@ -1,8 +0,0 @@
-import { Account, Client } from 'appwrite';
-
-const client = new Client();
-client.setProject('67b6c19200039343be3b');
-
-const account = new Account(client);
-
-export {account, client};
