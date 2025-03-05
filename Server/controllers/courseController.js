68cf36d330cf889034e0acdcb1d48f5657ff5e64
BHADRIKA <bhadrika2226@gmail.com>
2025-03-05 17:14:54 +0530

    Add moduleCourseFeatureId to courseCard query and implement getCourseDetails endpoint

--- Server/controllers/courseController.js
+++ Server/controllers/courseController.js
@@ -10,12 +10,28 @@ export const getCoursCard=async(req,res)=>{
         const courseCard=await AppwriteDB.listDocuments(
             process.env.APPWRITE_DBID,
             process.env.APPWRITE_DBC_Course,
-           [ Query.select(["title",'description','price','category','stars','validity','image'])]
+           [ Query.select(["title",'description','price','category','stars','validity','image','moduleCourseFeatureId'])]
         );
         console.log(courseCard.documents);
          res.status(200).json(courseCard.documents);
     }catch(error){
         console.log("data fetching error in getCourseCards ",error);
+        res.status(400).json({message:"Something went wrong"});
+    }
+}
+
+export const getCourseDetails=async(req,res)=>{
+    try{
+        console.log("Inside getCourseDetails");
+        const courseDetails=await AppwriteDB.getDocument(
+            process.env.APPWRITE_DBID,
+            process.env.APPWRITE_DBC_Course,
+            Query.select(["preTitle","title","subTitle","description","practiceExamples","realProjects","moduleId"])
+        );
+        console.log(courseDetails.documents);
+        res.status(200).json(courseDetails.documents);
+    }catch(error){
+        console.log("data fetching error in getCourseDetails ",error);
         res.status(400).json({message:"Something went wrong"});
     }
 }
